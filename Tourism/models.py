77cecf15efdc13e906102531189aca88631146b7
Yetunde05 <olawunmiolayinka235@gmail.com>
2022-12-10 12:28:23 +0100

    update

--- Tourism/models.py
+++ Tourism/models.py
@@ -8,30 +8,31 @@ class Packages(models.Model):
     image = models.ImageField(upload_to="img")
     price = models.DecimalField(decimal_places=2,max_digits=9)
 
-def __str__(self):
-    return self.country
+    def __str__(self):
+        return self.country
 
 class Destination(models.Model):
     countries = models.CharField(max_length=100)
     images = models.ImageField(upload_to="imgs")
-def __str__(self):
-    return self.countries
+    def __str__(self):
+        return self.countries
 
 class Tour_guides(models.Model):
     name = models.CharField(max_length=100)
     title = models.TextField()
     images1 = models.ImageField(upload_to="im")
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 class Testimonial(models.Model):
     images2 = models.ImageField(upload_to="ib")
     testimony = models.TextField()
     name = models.CharField(max_length=100)
     occupation = models.TextField()
-def __str__(self):
-    return self.images2
+    
+    def __str__(self):
+        return self.name
 
 
 
